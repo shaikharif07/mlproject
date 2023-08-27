934f33ef7eb0fe8e754a029d31a6df665d0a7fb4
Arif Shaikh <arif8108shaikh@gmail.com>
2023-08-27 19:51:12 +0530

    Data Tranformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 #dataclass is used to describe variables in a function
 @dataclass
 class DataIngestionConfig:
@@ -52,5 +55,8 @@ class DataInjection:
 
 if __name__=="__main__":
     obj = DataInjection()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_tranformation = DataTransformation()
+    data_tranformation.initiate_data_transformation(train_data, test_data)
 
